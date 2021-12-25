c269c1c1c035174d15ce35bc97e76c529a3dbc3c
Danilo Oliveira <danilo.ps101@gmail.com>
2021-12-25 06:35:59 -0300

    v1.0

--- modo.js
+++ modo.js
@@ -1,16 +1,17 @@
 let cont = 0
 
-let fundo = document.getElementById('luz')
+let fundo = document.getElementById('home')
+let luz = document.getElementById('luz')
 
 function troca(){
     cont ++
     
     if (cont % 2 == 1){//impar 
-        fundo.style.backgroundColor = '#3399ff'
-        fundo.style.transform = 'rotate(0deg)'
+        fundo.className = 'escuro'
+        luz.style.transform = 'rotate(0deg)'
     }else {
-        fundo.style.backgroundColor = 'white'
-        fundo.style.transform = 'rotate(180deg)'
+        fundo.className = 'claro'
+        luz.style.transform = 'rotate(180deg)'
     }
     console.log(cont)
 }
